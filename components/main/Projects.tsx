aa1c040e04cbcea9df08280139711dee15c15f42
Kuldik <112172212+Kuldik@users.noreply.github.com>
2025-10-23 15:23:26 +0200

    feat: add three new projects

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -12,6 +12,24 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full flex flex-col gap-10 px-10 text-3xl">
         <ProjectCard
+          src="./OP.jpg"
+          title="Organic people"
+          href="https://organic-people.com/blog/"
+          description="The project consisted of two interconnected parts. The first part was built with Astro and React.js (TS, SASS) for fast, static product pages. The second part utilized WordPress and PHP, integrated with the Directus API. The header navigation links to the blog — which belongs to the WordPress (PHP) part — serving as a central hub providing access to all other pages I developed. "
+        />
+        <ProjectCard
+          src="./Instoria.png"
+          title="Instoria Website"
+          description="This is multipage website build using HTML SASS, JS, PHP and WordPress. Also, in this website we use WooCommerce plugins as well. I did this project with my mentor. The task was to completely redesign the site and create additional functionality and pages in JS and PHP."
+          href="https://instoria.ru"
+        />
+        <ProjectCard
+          src="./history.gif"
+          title="Historical Dates"
+          description="Developed an interactive Historical Dates widget from scratch based on technical specifications and Figma designs. The component features a rotating timeline with six 5-year categories (1996-2025), synchronized year animations using GSAP, and a responsive event slider (Swiper). Built with React 18, TypeScript, and Sass, it ensures full accessibility and is designed as an independent, reusable block for multiple instances on a page."
+          href="https://kuldik.github.io/historical_dates/"
+        />
+        <ProjectCard
           src="./BankDash.png"
           title="BankDash app"
           href="https://kuldik.github.io/Dashboard_UI_Kit/"
@@ -40,12 +58,6 @@ const Projects = () => {
           title="Blog-project app"
           href="https://github.com/Kuldik/Blog-project"
           description="This app was build using React.js and MongoDB. It is a blog app where you can create, read, update and delete blog posts.And thanks to MongoDB server, u can register you account and log in it"
-        />
-        <ProjectCard
-          src="./Instoria.png"
-          title="Instoria Website"
-          description="This is multipage website build using HTML SASS, JS, PHP and WordPress. Also, in this website we use WooCommerce plugins as well. I did this project with my mentor. The task was to completely redesign the site and create additional functionality and pages in JS and PHP."
-          href="https://instoria.ru"
         />
       </div>
     </div>
